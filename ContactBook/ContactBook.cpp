c0dffdc37c5df06f83e94c8cde32e30f845d0364
antiestablishment <overpayin@protonmail.com>
2023-08-31 22:49:37 +0100

    All functions finished. Basic functionality complete, may improve add more functionally and improve the application aesthetically at a later date.

--- ContactBook/ContactBook.cpp
+++ ContactBook/ContactBook.cpp
@@ -13,7 +13,6 @@ struct aContact {
     char emailAddress[128];
     char phoneNumber[16];
     char homeAddress[128];
-    bool isDeleted = false;
 };
 
 bool directoryExists(const char*);
@@ -24,6 +23,9 @@ void fetchContact();
 void addContact();
 void listAllContacts();
 void clearInputBuffer();
+void editContact();
+void deleteContact();
+void exportContact();
 
 void clearInputBuffer() {
     int i;
@@ -40,7 +42,7 @@ bool directoryExists(const char* absolutePath) {
 }
 
 char displayMenu() {
-    // system("cls");
+    system("cls");
     int choiceChar;
     printf(" \n\n   Welcome to ant1terrorist's contact app!\n\n");
     printf("    Options:\n\n");
@@ -73,9 +75,22 @@ void performAction(char choiceChar) {
         fetchContact();
         choiceChar = ' ';
         break;
+    case('4'):
+        system("cls");
+        editContact();
+        choiceChar = ' ';
+        break;
+    case('5'):
+        system("cls");
+        deleteContact();
+        choiceChar = ' ';
+        break;
+    case('6'):
+        system("cls");
+        exportContact();
+        choiceChar = ' ';
+        break;
     case('Q'):
-        printf("\n\n    Exiting...");
-        exit(0);
     case('q'):
         printf("\n\n    Exiting...");
         exit(0);
@@ -87,33 +102,36 @@ void generateFile() {
     FILE* contactFile = fopen("contacts.bin", "ab");
     if (contactFile == NULL) {
         fprintf(stderr, "\nError opening file.\n");
+        printf("\n\n    Could not open a file or generate one.");
+        printf("\n    Make sure a file exists and isn't broken.");
+        printf("\n\n    Closing program...");
+        exit(0);
     }
     fclose(contactFile);
 }
 
 void fetchContact() {
     char searchName[64];
-    printf("\n\n");
-    printf("    First name of who would you like to search for: ");
+    printf("\n\n    Please enter the name of the contact you would like to see:  ");
     fgets(searchName, sizeof(searchName), stdin);
     searchName[strcspn(searchName, "\n")] = '\0';
     FILE* contactFile = fopen("contacts.bin", "rb");
     if (contactFile == NULL) {
         fprintf(stderr, "\nError opening file.\n");
+        return;
     }
     struct aContact fetchedContact {};
     while (fread(&fetchedContact, sizeof(fetchedContact), 1, contactFile)) {
         if (strcmp(fetchedContact.contactName, searchName) == 0) {
-            if (fetchedContact.isDeleted == false) {
-                printf("\n\n");
-                printf("    Contact Name: %s\n", fetchedContact.contactName);
-                printf("    Contact Email: %s\n", fetchedContact.emailAddress);
-                printf("    Contact Phone Number: %s\n", fetchedContact.phoneNumber);
-                printf("    Contact Home Address: %s\n", fetchedContact.homeAddress);
-            }
+            printf("\n\n");
+            printf("    Contact Name: %s\n", fetchedContact.contactName);
+            printf("    Contact Email: %s\n", fetchedContact.emailAddress);
+            printf("    Contact Phone Number: %s\n", fetchedContact.phoneNumber);
+            printf("    Contact Home Address: %s\n", fetchedContact.homeAddress);
+            break;
         }
         else {
-             printf("\n\n    No such contact. Try again.");
+            printf("\n\n    No such contact. Try again.");
         }
     }
     fclose(contactFile);
@@ -124,19 +142,19 @@ void addContact() {
     struct aContact newContact {};
     char firstName[32] = {};
     char lastName[32] = {};
-    printf("    Please enter the name of the contact: ");
+    printf("\n\n    Please enter the name of the contact:  ");
     fgets(newContact.contactName, sizeof(newContact.contactName), stdin);
     newContact.contactName[strcspn(newContact.contactName, "\n")] = '\0';
     printf("\n");
-    printf("    Please enter the email address of the contact: ");
+    printf("    Please enter the email address of the contact:  ");
     fgets(newContact.emailAddress, sizeof(newContact.emailAddress), stdin);
     newContact.emailAddress[strcspn(newContact.emailAddress, "\n")] = '\0';
     printf("\n");
-    printf("    Please enter the phone number of the contact: ");
+    printf("    Please enter the phone number of the contact:  ");
     fgets(newContact.phoneNumber, sizeof(newContact.phoneNumber), stdin);
     newContact.phoneNumber[strcspn(newContact.phoneNumber, "\n")] = '\0';
     printf("\n");
-    printf("    Please enter the home address of the contact: ");
+    printf("    Please enter the home address of the contact:  ");
     fgets(newContact.homeAddress, sizeof(newContact.homeAddress), stdin);
     newContact.homeAddress[strcspn(newContact.homeAddress, "\n")] = '\0';
     printf("\n");
@@ -155,13 +173,159 @@ void listAllContacts() {
     contactFile = fopen("contacts.bin", "rb");
     if (contactFile == NULL) {
         fprintf(stderr, "\nError opening file.\n");
+        return;
     }
+
     struct aContact listedContact {};
     printf("\n\n");
     while (fread(&listedContact, sizeof(listedContact), 1, contactFile)) {
-        printf("    Contact: %s\n", listedContact.contactName);
+            printf("    Contact: %s\n", listedContact.contactName);
     }
     fclose(contactFile);
+}
+
+void editContact() {
+    FILE* contactFile;
+    contactFile = fopen("contacts.bin", "rb+");
+    if (contactFile == NULL) {
+        fprintf(stderr, "\nError opening file.\n");
+        return;
+    }
+
+    struct aContact toEdit {};
+    bool foundContact = false;
+    char contactID[64];
+    printf("\n\n    Enter the name of the contact you would like to edit:  ");
+    fgets(contactID, sizeof(contactID), stdin);
+    contactID[strcspn(contactID, "\n")] = '\0';
+
+    while (fread(&toEdit, sizeof(toEdit), 1, contactFile)) {
+        if (strcmp(toEdit.contactName, contactID) == 0) {
+            foundContact = true;
+            printf("\n\n  Contact Found!\n");
+            printf("\n  Editing contact: %s\n", contactID);
+            printf("\n\n  Enter the new contact name (re-enter the current one if it hasn't changed):  ");
+            fgets(toEdit.contactName, sizeof(toEdit.contactName), stdin);
+            toEdit.contactName[strcspn(toEdit.contactName, "\n")] = '\0';
+            printf("\n    Contact Name Successfully edited!");
+            printf("\n\n    Enter the new contact email (re-enter the current one if it hasn't changed):  ");
+            fgets(toEdit.emailAddress, sizeof(toEdit.emailAddress), stdin);
+            toEdit.emailAddress[strcspn(toEdit.emailAddress, "\n")] = '\0';
+            printf("\n    Contact Email Address Successfully edited!");
+            printf("\n\n    Enter the new contact phone number (re-enter the current one if it hasn't changed):  ");
+            fgets(toEdit.phoneNumber, sizeof(toEdit.phoneNumber), stdin);
+            toEdit.phoneNumber[strcspn(toEdit.phoneNumber, "\n")] = '\0';
+            printf("\n    Contact Phone Number Successfully edited!");
+            printf("\n\n    Enter the new contact home address (re-enter the current one if it hasn't changed):  ");
+            fgets(toEdit.homeAddress, sizeof(toEdit.homeAddress), stdin);
+            toEdit.homeAddress[strcspn(toEdit.homeAddress, "\n")] = '\0';
+            printf("\n    Contact Home Address Successfully edited!");
+            fseek(contactFile, -(long)sizeof(toEdit), SEEK_CUR);
+            fwrite(&toEdit, sizeof(toEdit), 1, contactFile);
+            printf("\n\n    Contact edit saved successfully!");
+            break;
+        }
+    }
+    if (!foundContact) {
+        printf("\n  Contact Not Found.");
+    }
+    fclose(contactFile);
+}
+
+void deleteContact() {
+    FILE* contactFile;
+    contactFile = fopen("contacts.bin", "rb");
+    if (contactFile == NULL) {
+        fprintf(stderr, "\nError opening the contact file.\n");
+        return;
+    }
+
+    FILE* tempFile;
+    tempFile = fopen("contactTempStore.bin", "wb");
+    if (tempFile == NULL) {
+        fprintf(stderr, "\nError opening temporary contact storage file.\n");
+        return;
+    }
+
+    struct aContact tempStruct {};
+    bool foundContact = false;
+    char contactID[64];
+    char decisionCheck[3] = {};
+    const char* fileContacts = "contacts.bin";
+    const char* fileTemp = "contactTempStore.bin";
+
+    printf("\n\n    Enter the name of the contact you would like to delete:  ");
+    fgets(contactID, sizeof(contactID), stdin);
+    clearInputBuffer();
+    contactID[strcspn(contactID, "\n")] = '\0';
+
+
+    while (fread(&tempStruct, sizeof(aContact), 1, contactFile)) {
+        if (strcmp(tempStruct.contactName, contactID) != 0) {
+            fwrite(&tempStruct, sizeof(aContact), 1, tempFile);
+        }
+    }
+    printf("\n\n    Ready to delete %s...", contactID);
+    fclose(contactFile);
+    fclose(tempFile);
+
+    if (remove(fileContacts) == 0) {
+        if (rename(fileTemp, fileContacts) == 0) {
+            printf("\n\n    Contact %s has been deleted!", contactID);
+        }
+        else {
+            fprintf(stderr, "\nError renaming the temporary contact storage file.\n");
+        }
+    }
+    else {
+        fprintf(stderr, "\nError deleting the original contact storage file.\n");
+    }
+}
+
+void exportContact() {
+    char contactID[64];
+    bool foundContact = false;
+
+    FILE* contactFile;
+    contactFile = fopen("contacts.bin", "rb+");
+    if (contactFile == NULL) {
+        fprintf(stderr, "\nError opening contacts.bin for reading.\n");
+        return;
+    }
+
+    FILE* exportDest;
+    exportDest = fopen("exportedContact.txt", "w");
+    if (exportDest == NULL) {
+        fprintf(stderr, "\nError opening exportedContact.txt for writing.\n");
+        return;
+    }
+
+    struct aContact toExport {};
+
+    printf("\n\n    Enter the name of the contact you would like to export:  ");
+    fgets(contactID, sizeof(contactID), stdin);
+    contactID[strcspn(contactID, "\n")] = '\0';
+    while (fread(&toExport, sizeof(toExport), 1, contactFile)) {
+        if (strcmp(toExport.contactName, contactID) == 0) {
+            foundContact = true;
+            printf("\n\n    Contact found! \n");
+            printf("\n\n    Exporting contact to text file now... \n");
+            fprintf(exportDest, "\n\n  -- Contact Exported from ant1terrorists CLI contact book --\n\n");
+            fprintf(exportDest, "       Contact name: %s\n", toExport.contactName);
+            fprintf(exportDest, "       Contact name: %s\n", toExport.contactName);
+            fprintf(exportDest, "       Contact name: %s\n", toExport.contactName);
+            fprintf(exportDest, "       Contact name: %s\n", toExport.contactName);
+        }
+    }
+    if (!foundContact) {
+        printf("\n\n    Contact not found.");
+    }
+    else {
+        printf("\n\n    Contact exported succesfully!");
+    }
+
+    fclose(contactFile);
+    fclose(exportDest);
 }
 
 int main()
